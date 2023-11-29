39a22bbd004992ba29e85e7f0808436558e8fb84
FlipZ3ro <tokenla15@gmail.com>
2023-11-30 00:20:31 +0700

    first commit

---
+++ run.js
@@ -0,0 +1,45 @@
+const ethutils = require('ethereum-mnemonic-privatekey-utils');
+const bip39 = require('bip39');
+const { Account } = require('eth-lib/lib');
+const rl = require('readline-sync')
+const fs = require('fs')
+
+/*
+    0x Wallet Generator
+    Created by Viloid ( github.com/vsec7 )
+*/
+
+async function createWallet(){
+	const mnemonic = bip39.generateMnemonic();
+	const pk = '0x' + ethutils.getPrivateKeyFromMnemonic(mnemonic);
+	const acc = Account.fromPrivate(pk);
+	return {
+		'address': (acc.address).toLowerCase(), 
+		'pk': pk,
+		'mnemonic': mnemonic
+	}
+}
+
+async function generateWallet( n, o){
+	for (var i = 1; i <= n; i++) {
+		const wallet = await createWallet()
+		const data = `Address : ${wallet.address}\nPrivateKey: ${wallet.pk}\nMnemonic: ${wallet.mnemonic}\n`;
+		if(o){
+			fs.appendFile( o, data+'\n', (err) => { if(err) throw err; })
+		}
+		console.log(data)
+	}
+}
+
+(async () => {
+	console.log(`
++-----------------------------------------+
+            0X WALLET GENERATE
+          Create By me : arapzz
++-----------------------------------------+
+		`)
+	const n = rl.question('[?] How Many Wallet: ')
+	const o = rl.question('[?] Output (Type Name Location Backup): ')
+	console.log('\n')
+	await generateWallet( n, o)
+})();
